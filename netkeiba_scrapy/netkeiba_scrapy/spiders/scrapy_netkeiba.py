2377ee84c6ab70d0e65cfda5a24df1fb4e590b23
monda00 <monda0524@gmail.com>
2020-06-24 00:18:59 +0900

    ページを追ってURLを取得

--- netkeiba_scrapy/netkeiba_scrapy/spiders/scrapy_netkeiba.py
+++ netkeiba_scrapy/netkeiba_scrapy/spiders/scrapy_netkeiba.py
@@ -2,29 +2,38 @@
 import scrapy
 from netkeiba_scrapy.items import RaceUrl
 
-class ScrapyNetkeibaSpider(scrapy.Spider):
-    name = 'scrapy_netkeiba'
+class RaceUrlSpider(scrapy.Spider):
+    name = 'scrapy_race_url'
     allowed_domains = ['db.sp.netkeiba.com']
     base_url = 'https://db.sp.netkeiba.com'
-    page_num = 1
     start_year = 2019
     end_year = 2020
     start_urls = [
         base_url + '/?pid=race_list&start_year=' +
-        str(start_year) + '&end_year=' + str(end_year) + '&page=' + str(page_num)
+        str(start_year) + '&end_year=' + str(end_year) + '&page=1'
     ]
 
     def parse(self, response):
-
+        """
+        検索ページのレース一覧からレースのURLを取得する
+        """
         urls = response.xpath('//div[@class="LinkBox_01 fc"]/a/@href').extract()
         for url in urls:
             yield RaceUrl(url=url)
+
+        if self.exist_next_page(response):
+            next_page_url = self.base_url + response.xpath(
+                '//div[@class="Common_Pager"]//a[@title="次へ"]/@href').extract_first()
+            yield scrapy.Request(next_page_url, callback=self.parse)
+        else:
+            return
 
     def exist_next_page(self, response):
         """
         次のページがあるか判定
         「次へ」にhrefがあるかで判定する
         """
-        exist = False
-
-        return exist
+        if response.xpath('//div[@class="Common_Pager"]//a[@title="次へ"]/@href').extract_first() is None:
+            return False
+        else:
+            return True

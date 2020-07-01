ac5810a07b14ab2f048cfa404b6e801ea9365745
monda00 <monda0524@gmail.com>
2020-07-02 00:33:10 +0900

    レース情報から各馬の情報を取得

--- netkeiba_scrapy/netkeiba_scrapy/spiders/scrapy_netkeiba.py
+++ netkeiba_scrapy/netkeiba_scrapy/spiders/scrapy_netkeiba.py
@@ -49,7 +49,7 @@ class Horse(scrapy.Spider):
         csvファイルからurlのリストを取得
         """
         urls = []
-        with open('../../url.csv') as f:
+        with open('./url.csv') as f:
             reader = csv.reader(f)
             for row in reader:
                 if not row[0] == 'url':
@@ -62,9 +62,59 @@ class Horse(scrapy.Spider):
         """
         レースページから各馬の情報を取得
         """
+        rank = 1
 
-    def exist_next_rank():
+        while self.exist_next_rank(response, rank):
+            name = response.xpath(
+                '//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]/td[4]/a/text()'.format(rank)).extract_first()
+            race_name = response.xpath(
+                '//span[@class="RaceName_main"]/text()').extract_first()
+            race_date = response.xpath(
+                '//span[@class="Race_Date"]/text()').extract_first().replace('\n', '')
+            frame_number = response.xpath(
+                '//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]/td[2]/text()'.format(rank)).extract_first()
+            horse_number = response.xpath(
+                '//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]/td[3]/text()'.format(rank)).extract_first()
+            age = response.xpath(
+                '//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]/td[5]/text()'.format(rank)).extract_first()
+            weight = response.xpath(
+                '//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]/td[6]/text()'.format(rank)).extract_first()
+            jockey = response.xpath(
+                '//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]/td[7]/a/text()'.format(rank)).extract_first()
+            time = response.xpath(
+                '//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]/td[8]/text()'.format(rank)).extract_first()
+            agari = response.xpath(
+                '//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]/td[12]/text()'.format(rank)).extract_first()
+            win = response.xpath(
+                '//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]/td[13]/text()'.format(rank)).extract_first()
+            popular = response.xpath(
+                '//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]/td[14]/text()'.format(rank)).extract_first()
+            horse_weight = response.xpath(
+                '//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]/td[15]/text()'.format(rank)).extract_first()
+
+            yield Horse(
+                name=name,
+                race_name=race_name,
+                race_date=race_date,
+                frame_number=frame_number,
+                horse_number=horse_number,
+                age=age,
+                weight=weight,
+                jockey=jockey,
+                time=time,
+                agari=agari,
+                win=win,
+                popular=popular,
+                horse_weight=horse_weight,
+                rane=rank
+            )
+            rank += 1
+
+    def exist_next_rank(self, response, rank):
         """
         レースの結果一覧の次の順位があるか
         """
-        return False
+        if response.xpath('//table[@class="table_slide_body ResultsByRaceDetail"]//tr[{}]'.format(rank)).extract_first() is None:
+            return False
+        else:
+            return True

a8d0fa0b7259b60ae26d255008de9d36c25cdd03
monda00 <monda0524@gmail.com>
2020-07-01 21:54:37 +0900

    add horse item

--- netkeiba_scrapy/netkeiba_scrapy/items.py
+++ netkeiba_scrapy/netkeiba_scrapy/items.py
@@ -10,3 +10,19 @@ import scrapy
 
 class RaceUrl(scrapy.Item):
     url = scrapy.Field()
+
+class Horse(scrapy.Item):
+    name = scrapy.Field()
+    race_name = scrapy.Field()
+    race_date = scrapy.Field()
+    frame_number = scrapy.Field()
+    horse_number = scrapy.Field()
+    age = scrapy.Field()
+    weight = scrapy.Field()
+    jockey = scrapy.Field()
+    time = scrapy.Field()
+    agari = scrapy.Field()
+    win = scrapy.Field()
+    popular = scrapy.Field()
+    horse_weight = scrapy.Field()
+    rank = scrapy.Field()
